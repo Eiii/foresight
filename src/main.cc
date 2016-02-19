322777836f02bc08d2d4e1f1711f869abd9135e7
Erich Merrill III <erich@erichmerrill.com>
2016-02-18 16:49:34 -0800

    Initial commit - skeleton development environment

---
+++ src/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "foresight/test.h"
+
+int main() {
+  std::cout << "Test." << std::endl;
+  std::cout << test_fn(7) << std::endl;
+  return 0;
+}
---
+++ src/test.cc
@@ -0,0 +1,8 @@
+#include "foresight/test.h"
+
+#include <iostream>
+
+int test_fn(int x) {
+  if (x > 10) return x/2;
+  else return x*2;
+}
