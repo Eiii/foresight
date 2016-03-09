c0bd88c31d6ae4818677e3c7d1af69cc1611074b
Erich Merrill III <erich@erichmerrill.com>
2016-03-08 17:05:38 -0800

    Added some TODO planning

--- src/experimentactiontype.cc
+++ src/experimentactiontype.cc
@@ -2,6 +2,7 @@
 
 namespace fore {
 
+//TODO: Add model info
 ExperimentActionType::ExperimentActionType(
   ActionType::Id id, std::string name, Duration duration, bool cancelable,
   Resource::Amount requires, 
@@ -13,17 +14,24 @@ ExperimentActionType::ExperimentActionType(
 std::vector<Action::Ptr> 
     ExperimentActionType::GenerateActions(const State& state) const
 {
+  //TODO: Generate 'base' action
+  //TODO: Calculate best input point on model
+  //TODO: Build/return resultant action 
   return ActionType::GenerateActions(state);
 }
 
 State ExperimentActionType::Start(
     const Action& action, const State& state) const
 {
+  //TODO: Update model w/ false observation
   return ActionType::Start(action, state);
 }
 
 void ExperimentActionType::End(const Action& action, StateFactory* fact) const
 {
+  //TODO: 'base' End
+  //TODO: Remove false observation
+  //TODO: Calculate+add model observation
   ActionType::End(action, fact);
 }
 
