c988fc1aabd714c0fa35a708a9994b106e7a88c2
Erich Merrill III <erich@erichmerrill.com>
2016-03-15 20:32:36 -0700

    Added dropped files

---
+++ include/foresight/bayesopt/custommodel.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "bayesopt.hpp"
+
+namespace fore {
+
+class CustomModel : public bayesopt::DiscreteModel {
+  //Type aliases
+  public:
+    using Function = double (*) (const vectord& point);
+
+  //Constructors
+  public:
+    CustomModel(const vecOfvec& valid_set, bopt_params&& params,
+                Function fn, double epsilon);
+    virtual ~CustomModel();
+    //Forbid copying
+    CustomModel(const CustomModel& rhs) = delete;
+    CustomModel& operator=(const CustomModel& rhs) = delete;
+    //Allow moving
+    CustomModel(CustomModel&& rhs) = default;
+    CustomModel& operator=(CustomModel&& rhs) = default;
+
+  //Base functions
+  public:
+    virtual double evaluateSample(const vectord& Xi) override;
+    virtual bool checkReachability(const vectord& query) override;
+
+  //Static functions
+  public:
+    static bopt_params DefaultParams(int seed);
+    static vecOfvec DefaultValidSet();
+    static void DeleteParams(bopt_params* params);
+
+  //Backing functions
+  public:
+    static double CosineFn(const vectord& Xi);
+
+  //Getters
+  public:
+    const Function& function() { return function_; }
+    double epsilon() { return epsilon_; }
+
+  private:
+    bopt_params params_;
+    Function function_;
+    double epsilon_;
+};
+
+}
---
+++ src/bayesopt/custommodel.cc
@@ -0,0 +1,94 @@
+#include "foresight/bayesopt/custommodel.h"
+
+namespace fore {
+
+CustomModel::CustomModel(const vecOfvec& valid_set, bopt_params&& params,
+                         Function fn, double epsilon) :
+    DiscreteModel(valid_set, params), params_(params), function_(fn), 
+    epsilon_(epsilon) {}
+
+CustomModel::~CustomModel()
+{
+  CustomModel::DeleteParams(&params_);
+}
+
+double CustomModel::evaluateSample(const vectord& Xi) 
+{
+  return function_(Xi);
+}
+
+bool CustomModel::checkReachability(const vectord& query)
+{
+  //TODO: Maybe check that query is in the valid set?
+  (void)query;
+  return true;
+}
+
+bopt_params CustomModel::DefaultParams(int seed)
+{
+  bopt_params params = initialize_parameters_to_default();
+
+  set_criteria(&params, "cEI");
+  params.n_crit_params = 0;
+
+  set_kernel(&params,"kSEISOfixed");
+  params.kernel.hp_mean[0] = 1.0;
+  params.kernel.hp_std[0] = 1.0;
+  params.kernel.n_hp = 1;
+
+  set_mean(&params,"mConst"); //Mean 0, std 1
+  params.mean.coef_mean[0] = 0.0;
+  params.mean.coef_std[0] = 1.0;
+  params.mean.n_coef = 1;
+
+  set_surrogate(&params,"sGaussianProcessML");
+  params.noise = 1e-12;
+
+  params.sc_type = SC_MTL; //Kernel parameter learning type
+  params.l_type = L_EMPIRICAL;
+  params.l_all = 0;
+
+  //TODO this is just so that the initial observations are always the same
+  params.random_seed = seed; 
+  params.n_iterations = 100; // Number of iterations - Not relevant
+  params.n_init_samples = 5;
+  params.init_method = 3; //Uniform samples
+  params.n_iter_relearn = 1; //TODO ???
+  params.force_jump = 0; //Don't jump
+
+  params.verbose_level = -1;
+
+  return params;
+}
+
+vecOfvec CustomModel::DefaultValidSet()
+{
+  //TODO: fill me!
+  vecOfvec valid_set;
+  return valid_set;
+}
+
+void CustomModel::DeleteParams(bopt_params* params)
+{
+  delete[] params->kernel.name;
+  delete[] params->mean.name;
+  delete[] params->log_filename;
+  delete[] params->load_filename;
+  delete[] params->save_filename;
+  delete[] params->surr_name;
+  delete[] params->crit_name;
+}
+
+double CustomModel::CosineFn(const vectord& Xi)
+{
+  double val;
+  val = 3.0 - (1.0 / 3.0) *
+        (pow(1.6 * Xi(0) - 0.5, 2.0) +
+         pow(1.6 * Xi(1) - 0.5, 2.0) -
+         0.3 * cos(3.0 * M_PI * (1.6 * Xi(0) - 0.5)) -
+         0.3 * cos(3.0 * M_PI * (1.6 * Xi(1) - 0.5))
+        );
+  return -val;
+}
+
+}
